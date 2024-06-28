8fdff653953f9ceb1208b2393b02c3adfc5a485e
Hyunho <sailor3947@gmail.com>
2024-06-28 11:56:42 +0900

    add: local data path

--- official/benchmark/bert_squad_benchmark.py
+++ official/benchmark/bert_squad_benchmark.py
@@ -41,13 +41,13 @@ from official.utils.misc import keras_utils
 
 """add: local data paths"""
 # pylint: disable=line-too-long
-PRETRAINED_CHECKPOINT_PATH = '/data/uncased_L-24_H-1024_A-16/bert_model'
-SQUAD_TRAIN_DATA_PATH = '/data/squad_v1.1/squad/v1.1/3.0.0/squad-train.tfrecord'
-SQUAD_PREDICT_FILE = '/data/squad_v1.1/squad/v1.1/3.0.0/squad-validation.tfrecord'
+PRETRAINED_CHECKPOINT_PATH = '/data/uncased_L-24_H-1024_A-16/bert_model.ckpt'
+SQUAD_TRAIN_DATA_PATH = '/data/squad_v1.1/squad/processed/squad-train.tfrecord'
+SQUAD_PREDICT_FILE = '/data/squad_v1.1/squad/downloads/rajpurkar_SQuAD-explorer_dev-v1.1lapqUtXWpzVWM2Z1PKUEkqZYAx2nTzAaxSOLA5Zpcsk.json'
 SQUAD_VOCAB_FILE = '/data/uncased_L-24_H-1024_A-16/vocab.txt'
-SQUAD_MEDIUM_INPUT_META_DATA_PATH = '/data/squad_v1.1/squad/v1.1/3.0.0/dataset_info.json'
-SQUAD_LONG_INPUT_META_DATA_PATH = '/data/squad_v1.1/squad/v1.1/3.0.0/dataset_info.json'
-SQUAD_FULL_INPUT_META_DATA_PATH = '/data/squad_v1.1/squad/v1.1/3.0.0/dataset_info.json'
+SQUAD_MEDIUM_INPUT_META_DATA_PATH = '/data/squad_v1.1/squad/processed/squad_full_meta_data'
+SQUAD_LONG_INPUT_META_DATA_PATH = '/data/squad_v1.1/squad/processed/squad_full_meta_data'
+SQUAD_FULL_INPUT_META_DATA_PATH = '/data/squad_v1.1/squad/processed/squad_full_meta_data'
 MODEL_CONFIG_FILE_PATH = '/data/uncased_L-24_H-1024_A-16/bert_config.json'
 # pylint: enable=line-too-long
 
